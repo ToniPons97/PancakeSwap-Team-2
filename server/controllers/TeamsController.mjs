e25a4c255b2273acf4d73257d9fc86e20f0a2970
Antonio Pons <toninopons@gmail.com>
2023-02-23 14:20:50 +0100

    TeamsController working correctly.

--- server/controllers/TeamsController.mjs
+++ server/controllers/TeamsController.mjs
@@ -3,16 +3,15 @@ import db from '../db/conn.mjs';
 const { TEAMS_COLL } = process.env;
 const collection = db.collection(TEAMS_COLL);
 
-console.log(TEAMS_COLL);
-console.log(collection);
-
+// Get a list of 50 NFTs max.
 const getAllTeams = async (req, res) => {
-    let result = collection.find({})
-        .limit(10)
-        .toArray()
-    
+    let result = await collection.find({})
+        .limit(50)
+        .toArray();
+        
     res.json(result).status(200);
-}
+};
+
 
 export {
     getAllTeams
